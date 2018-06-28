86846fb4e3f72aeb30a958a0138f01eed1c602cd
Levi Bergevin <39744439+scifiguy01@users.noreply.github.com>
2018-06-27 19:14:42 -0600

    Add files via upload

---
+++ gradeAverage.cpp
@@ -0,0 +1,40 @@
+/* https://www.codecademy.com/courses/learn-java/projects/grades-analyzer
+This program analyzes grades and finds averages. The point of this program
+is to show the ability to use ArrayList.*/
+import java.util.ArrayList;
+
+class GradeAnalyzer {
+  public GradeAnalyzer() {
+    
+  }
+  public int getAverage(ArrayList<Integer> grades) {
+    
+    if(grades.size() == 0) {
+      System.out.println("List is Empty");
+      return 0;
+    }
+    else {
+      int sum = 0;
+      for( Integer grade : grades ) {
+        sum = sum + grade;
+      }
+      int average = sum / grades.size();
+      System.out.println(average);
+      return average;
+    }
+  }
+  public static void main( String[] args ) {
+    ArrayList<Integer> myClassroom = new ArrayList<Integer>();
+    myClassroom.add(98);
+    myClassroom.add(92);
+    myClassroom.add(88);
+    myClassroom.add(75);
+    myClassroom.add(61);
+    myClassroom.add(89);
+    myClassroom.add(95);
+    GradeAnalyzer myAnalyzer = new GradeAnalyzer();
+    myAnalyzer.getAverage(myClassroom);
+    
+  }
+    
+}
